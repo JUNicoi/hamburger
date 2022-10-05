69166e4739173dedb9994d9d89eab91b1fff868b
JUN.inoue <nico.i.works@gmail.com>
2022-10-05 22:33:29 +0900

    修正

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 $(".openbtn").click(function () {
   //ボタンがクリックされたら
   $(this).toggleClass("active"); //.openbtnに.activeを付与
+  $("#gnav").toggleClass("panelactive"); //#gnaveに.panelactiveを付与
+});
+
+$("#gnav a").click(function () {
+  //リンクがクリックされたら
+  $(".openbtn").removeClass("active"); //.openbtnから.activeを削除
+  $("#gnav").removeClass("panelactive"); //#gnavから.panelactiveも削除
 });
